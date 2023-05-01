b0e86c3deb647f3070b07efb80bb9af9e5238e05
Bouchra Akl <bushraakl1234@gmail.com>
2023-05-01 11:13:39 -0300

    testing and execution done

--- src/main/java/org/cryptography/RSA.java
+++ src/main/java/org/cryptography/RSA.java
@@ -5,15 +5,26 @@ package org.cryptography;
 
 import javax.crypto.Cipher;
 import javax.crypto.NoSuchPaddingException;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.NoSuchAlgorithmException;
-import java.security.PublicKey;
+import java.security.*;
 
 /*--------------------------------------------*/
 public class RSA {
 
     private static final int KEY_SIZE = 2048;
+
+    public static void main(String[] args) throws Exception {
+        KeyPair keyPair = generateKeyPair();
+
+        String originalMessage = "Mensagem original para ser criptografada.";
+        byte[] encryptedMessage = criptografar(originalMessage, keyPair.getPublic());
+        String dycriptedMessage = descriptografar(encryptedMessage, keyPair.getPrivate());
+
+        System.out.println("Mensagem original: " + originalMessage);
+        System.out.println("Mensagem criptografada: " + new String(encryptedMessage, "UTF-8"));
+        System.out.println("Mensagem descriptografada: " + dycriptedMessage);
+
+    }
+
 
     private static KeyPair generateKeyPair() throws Exception {
 
@@ -33,10 +44,10 @@ public class RSA {
 
     }
 
-    private static String descriptografar(byte[] messageEncripted, PublicKey publicKey) throws Exception {
+    private static String descriptografar(byte[] messageEncripted, PrivateKey privateKey) throws Exception {
 
         Cipher cipher = Cipher.getInstance("RSA");
-        cipher.init(Cipher.DECRYPT_MODE, publicKey);
+        cipher.init(Cipher.DECRYPT_MODE, privateKey);
 
         byte[] bytesDescripted = cipher.doFinal(messageEncripted);
 
